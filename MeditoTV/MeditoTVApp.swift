2479272629c83d5873af147f1ac714d81d4dd504
mlukacs <mlukacs@deezer.com>
2022-12-23 20:03:58 +0100

    Initial Commit

---
+++ MeditoTV/MeditoTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditoTVApp.swift
+//  MeditoTV
+//
+//  Created by Martin Lukacs on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditoTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
